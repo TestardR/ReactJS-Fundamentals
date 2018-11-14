bfd7b850f4c82e757645c9b9694e57bc13371c24
Romain <rmtestard@gmail.com>
2018-11-14 14:13:15 +0100

    AuthorQuiz

---
+++ AuthorQuiz/authorquiz/src/AuthorQuiz.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AuthorQuiz from './AuthorQuiz';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+describe('Author Quiz', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<AuthorQuiz />, div);
+  });
+});
